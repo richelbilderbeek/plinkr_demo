9f059e14c79c8d5f50e0852dc0bb22d1201c7786
richelbilderbeek <richel@richelbilderbeek.nl>
2021-07-28 10:21:54 +0200

    Call script outside of container

--- demo_container.sh
+++ demo_container.sh
@@ -2,4 +2,4 @@
 singularity pull library://richelbilderbeek/default/plinkr:v0.15.1
 singularity run-help plinkr_v0.15.1.sif
 singularity inspect --deffile plinkr_v0.15.1.sif
-singularity exec plinkr_v0.15.1.sif sh -c "Rscript demo_container.R"
+singularity exec plinkr_v0.15.1.sif Rscript demo_container.R
